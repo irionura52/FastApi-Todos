090e1e801018eb671e218ae8d2144534d640fec3
irionura52 <kdhtop0@naver.com>
2025-05-13 13:57:53 +0000

    10thweekupdatemainpy

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -4,12 +4,12 @@ from pydantic import BaseModel
 from typing import Optional
 import json
 import os
-#from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 # Prometheus 메트릭스 엔드포인트 (/metrics)
-#Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 
 # To-Do 항목 모델
